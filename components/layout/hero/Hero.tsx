014f0448cd7db17c245f144272aa56019bf043d8
Jeramiah Coffey <jerrygcoffey@gmail.com>
2023-03-20 16:53:00 -0500

    style: continue styling Navbar and Hero

--- components/layout/hero/Hero.tsx
+++ components/layout/hero/Hero.tsx
@@ -1,24 +1,20 @@
-import headerImage from '@/public/assets/home/mobile/image-header.jpg';
+import mobileHeaderImage from '@/public/assets/home/mobile/image-header.jpg';
+import tabletHeaderImage from '@/public/assets/home/tablet/image-header.jpg';
 import styles from './Hero.module.scss';
 
 import Button from '@/components/inputs/button/Button';
 import useViewport from '@/hooks/useViewport';
 import Image from 'next/image';
-import { useEffect } from 'react';
 
 const Hero = () => {
   const { width } = useViewport();
-
-  useEffect(() => {
-    console.log(width);
-  }, [width]);
 
   return (
     <div className={styles.hero}>
       <div className={styles.imageContainer}>
         <Image
           className={styles.image}
-          src={headerImage}
+          src={width < 400 ? mobileHeaderImage : tabletHeaderImage}
           alt="Featured headphones"
           fill
         />
