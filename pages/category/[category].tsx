80435b59d5106eef920ce58fefbfab6065528015
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-03-22 18:18:58 -0500

    feat: fetch products by category

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,31 +1,66 @@
 import CategoryPageCard from '@/components/cards/productCards/CategoryPageCard';
 import ShopCardContainer from '@/components/containers/shopCardContainer/ShopCardContainer';
 import CategoryLayout from '@/components/layout/CategoryLayout';
+import { prisma } from '@/db/prismadb';
+import { GetStaticProps } from 'next';
 import { ReactNode } from 'react';
 import styles from './Category.module.scss';
 
-const Category = () => {
+type Product = {
+  id: string;
+  category: string;
+  name: string;
+  description: string;
+  image: { desktop: string; mobile: string; tablet: string };
+  new: boolean;
+};
+
+interface ICategoryPageProps {
+  products: Product[];
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { category: 'headphones' } },
+      { params: { category: 'speakers' } },
+      { params: { category: 'earphones' } },
+    ],
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<{ products: Product[] }> = async (
+  context
+) => {
+  const products = await prisma.product.findMany({
+    where: {
+      category: context.params?.category as string,
+    },
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+const Category = ({ products }: ICategoryPageProps) => {
+  console.log(products);
+
   return (
     <div className={styles.category}>
       <div className={styles.cards} id="categoryPageProductCards">
-        <CategoryPageCard
-          name="xx99 mark ii headphones"
-          image="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-          description="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
-          isNew={true}
-        />
-        <CategoryPageCard
-          name="xx99 mark ii headphones"
-          image="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-          description="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
-          isNew={true}
-        />
-        <CategoryPageCard
-          name="xx99 mark ii headphones"
-          image="/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
-          description="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
-          isNew={true}
-        />
+        {products.map((product) => (
+          <CategoryPageCard
+            key={product.id}
+            name={product.name}
+            image={product.image.desktop.slice(1)}
+            description={product.description}
+            isNew={product.new}
+          />
+        ))}
       </div>
       <ShopCardContainer />
     </div>
