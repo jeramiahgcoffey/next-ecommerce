00f592b9dc131fca5f5840a33b87c1d4cef85dc5
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-04-06 15:42:15 -0500

    feat: implement Checkout validation and formatting

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -4,7 +4,7 @@ import Back from '@/components/inputs/back/Back';
 import useCart from '@/hooks/useCart';
 import styles from '@/styles/Checkout.module.scss';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export interface IFormFields {
   billing: {
@@ -42,7 +42,6 @@ const Checkout = () => {
   const { cart } = useCart();
 
   const [errors, setErrors] = useState(initialErrors);
-  const [hasErrors, setHasErrors] = useState(false);
 
   const [formFields, setFormFields] = useState<IFormFields>({
     billing: {
@@ -64,82 +63,76 @@ const Checkout = () => {
   });
 
   const validateFields = () => {
-    setErrors(initialErrors);
+    let currentErrors = { ...initialErrors };
     for (const section in formFields) {
       const formSection = formFields[section as keyof IFormFields];
       for (const field in formSection) {
         const value = formSection[field as keyof typeof formSection] as string;
         if (value.length >= 115) {
-          setErrors((prev) => ({
-            ...prev,
+          currentErrors = {
+            ...currentErrors,
             [field]: 'Field must have less than 115 characters',
-          }));
+          };
         }
         if (field === 'email') {
           const regex =
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           if (!regex.test(value)) {
-            setErrors((prev) => ({
-              ...prev,
+            currentErrors = {
+              ...currentErrors,
               [field]: 'The email you entered is invalid',
-            }));
+            };
           }
         }
         if (field === 'zip') {
           const regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
           if (!regex.test(value)) {
-            setErrors((prev) => ({
-              ...prev,
+            currentErrors = {
+              ...currentErrors,
               [field]: 'The ZIP code you entered is invalid',
-            }));
+            };
           }
         }
         if (field === 'eNumber') {
           if (value.length !== 9) {
-            setErrors((prev) => ({
-              ...prev,
+            currentErrors = {
+              ...currentErrors,
               [field]: 'The eNumber must be 9 digits',
-            }));
+            };
           }
         }
         if (field === 'pin') {
           if (value.length !== 4) {
-            setErrors((prev) => ({
-              ...prev,
+            currentErrors = {
+              ...currentErrors,
               [field]: 'The PIN must be 4 digits',
-            }));
+            };
           }
         }
 
         if (!value) {
-          setErrors((prev) => ({
-            ...prev,
+          currentErrors = {
+            ...currentErrors,
             [field]: 'Field cannot be empty',
-          }));
+          };
         }
       }
     }
 
     if (formFields.payment.method !== 'e-money') {
-      setErrors((prev) => ({
-        ...prev,
+      currentErrors = {
+        ...currentErrors,
         eNumber: '',
         pin: '',
-      }));
+      };
     }
+
+    setErrors(currentErrors);
+    return Object.values(currentErrors).every((field) => field === '');
   };
 
-  useEffect(() => {
-    if (Object.values(errors).every((field) => field === '')) {
-      setHasErrors(false);
-    } else {
-      setHasErrors(true);
-    }
-  }, [errors]);
-
   const handleSubmit = () => {
-    validateFields();
-    console.log(hasErrors ? 'errors' : 'valid');
+    console.log(validateFields() ? 'valid' : 'errors');
   };
 
   return (
